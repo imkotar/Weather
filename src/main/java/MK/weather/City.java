26c5d12c13d3f0f864e5f82eb3edea5b1a68fe82
imkotar <48478734+imkotar@users.noreply.github.com>
2019-04-25 15:51:02 +0200

    Add files via upload

--- src/main/java/MK/weather/City.java
+++ src/main/java/MK/weather/City.java
@@ -1,24 +1,28 @@
 package MK.weather;
 
+
 public class City {
 
     private String city;
     private String temperature;
     private String longitude;
     private String latitude;
+    private String pressure;
+
+    ApiReader read = new ApiReader();
 
     public String getCity() { return city; }
 
     public void setCity(String city) { this.city = city; }
 
     public String getTemperature() {
-        ApiReader read = new ApiReader();
-        return read.readApi(getCity());
+        return read.readApi(0, getCity());
     }
+
     public void setTemperature(String temperature) { this.temperature = temperature; }
 
     public String getLongitude() {
-        return longitude;
+        return read.readApi(1, getCity());
     }
 
     public void setLongitude(String longitude) {
@@ -26,10 +30,16 @@ public class City {
     }
 
     public String getLatitude() {
-        return latitude;
+        return read.readApi(2, getCity());
     }
 
     public void setLatitude(String latitude) {
         this.latitude = latitude;
     }
+
+    public String getPressure() {
+        return read.readApi(3, getCity());
+    }
+
+    public void setPressure(String pressure) { this.pressure = pressure; }
 }
